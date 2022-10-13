5a477d41d62445fa947df2052402890fa19d6820
singh janmanpreet <janmanpreet.singh@gmail.com>
2022-10-13 22:00:18 +0200

    added welcome component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { HelloWorld } from "./HelloWorld";
+import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
     render() {
         return (
+            <>
                 <HelloWorld />
+                <Welcome name="jaman"/>
+            </>
         )
     }
 }
