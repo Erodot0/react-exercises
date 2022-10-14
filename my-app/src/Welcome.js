f58391b3e5b20949c560064369c481ece2e677b7
singh janmanpreet <janmanpreet.singh@gmail.com>
2022-10-14 09:48:43 +0200

    added age prop to welcome.js

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -3,9 +3,14 @@ import React from "react";
 
 export class Welcome extends React.Component {
     render() {
-        return ( this.props.name 
-                ? <p>Welcome {this.props.name}!</p>
-                : <p>Welcome user!</p>
+        return (
+            <div>
+                {this.props.name
+                    ? <p>Welcome {this.props.name}!</p>
+                    : <p>Welcome user!</p>
+                }
+                <p>Your age is {this.props.age}</p>
+            </div>
         )
     }
 }
