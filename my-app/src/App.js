6f2d1ab2dbf6e6ad55be44318493b3764d9f0c6a
singh janmanpreet <janmanpreet.singh@gmail.com>
2022-10-14 10:12:46 +0200

    added jsx expression in app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { HelloWorld } from "./HelloWorld";
 import { Welcome } from "./Welcome";
 
+const name = <strong>Jaman</strong>
+
 export class App extends React.Component {
     render() {
         return (
             <div>
                 <HelloWorld />
-                <Welcome name="jaman" age="21"/>
+                <Welcome name={name} age="21"/>
             </div>
         )
     }
