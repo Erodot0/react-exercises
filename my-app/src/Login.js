dc9aa071d3630efaffa3737fea27d89d1f97c7d3
singh janmanpreet <janmanpreet.singh@gmail.com>
2022-10-17 16:28:49 +0200

    bug fixes

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -1,27 +1,21 @@
 import React from "react";
 
 export class Login extends React.Component {
+    // creating state
     state = {
         username: "",
         password: "",
-        toRemeber: false
+        remember: false
     }
+    // handling events
+    handleInputChange = (event) => {
+        const value = event.target.value
+        const name = event.target.name
+        const type = event.target.type
+        const checked = event.target.checked
 
-    handleNameChange = (event) => {
         this.setState({
-            username: event.target.value
-        })
-    }
-
-    handlePasswordChange = (event) => {
-        this.setState({
-            password: event.target.value
-        })
-    }
-
-    handleCheckChange = (event) => {
-        this.setState({
-            toRemeber: event.target.value
+            [name]: type === "checkbox" ? checked : value
         })
     }
 
@@ -29,11 +23,11 @@ export class Login extends React.Component {
         return (
             <form>
                 <p>Username</p>
-                <input type="text" name="username" value={this.state.username} onChange={this.handleNameChange} />
+                <input type="text" name="username" value={this.state.username} onChange={this.handleInputChange} />
                 <p>Password</p>
-                <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange} />
+                <input type="password" name="password" value={this.state.password} onChange={this.handleInputChange} />
                 <p>Remeber</p>
-                <input type="checkbox" name="checkbox" value={this.state.password} onChange={this.handleCheckChange} />
+                <input type="checkbox" name="remember" checked={this.state.remember}  onChange={this.handleInputChange}/>
             </form>
         )
     }
