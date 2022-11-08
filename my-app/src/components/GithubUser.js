11bd0f8a688c13bc6687b890173762dc1a0e90da
singh janmanpreet <janmanpreet.singh@gmail.com>
2022-11-08 13:31:33 +0100

    costom-hooks-04 done

--- my-app/src/components/GithubUser.js
+++ my-app/src/components/GithubUser.js
@@ -1,14 +1,17 @@
 import { useGithubUser } from "./useGithubUser"
 
-export function GithubUser({username}){
-    const { data } = useGithubUser(username)
+export function GithubUser({ username }) {
+    const { data, loading, error } = useGithubUser(username)
 
-    return(
+    return (
         <div>
-            <div className="fetchContent">
+            {loading && <h1>Loading...</h1>}
+            {error && <h1>there was an error!</h1>}
+            {data && <div className="fetchContent">
                 {data.avatar_url && <a href={data.html_url}><img src={data.avatar_url} alt="userpfp" /></a>}
-                <h1>{data.name}</h1>                
+                <h1>{data.name}</h1>
             </div>
+            }
         </div>
     )
 }
--- my-app/src/components/useGithubUser.js
+++ my-app/src/components/useGithubUser.js
@@ -1,12 +1,26 @@
 import { useEffect, useState } from "react"
 
 export function useGithubUser(username){
-    const [data, setData] = useState("")
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(null)
+
 
     const fetchData = async (username) => {
-        const res = await fetch(`https://api.github.com/users/${username}`)
-        const resJson = await res.json()
-        setData(resJson)
+        setLoading(true)
+        setError(null)
+        try {
+            const res = await fetch(`https://api.github.com/users/${username}`)
+            const resJson = await res.json()
+            if(res.status !== 200){
+                setError(new Error("Errore"))
+            }
+            setData(resJson)
+        } catch (error) {
+            setError(error)
+        } finally {
+            setLoading(false)
+        }
     }
     
     useEffect(() => {
@@ -14,7 +28,9 @@ export function useGithubUser(username){
     },[username])
 
     return {
-        data: data,
-        fetchData: fetchData,
+            data: data,
+            error: error,
+            loading: loading,
+            fetchData: fetchData,
     }
 }
