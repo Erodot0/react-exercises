c0131e0c9eac6ffa44e7428157ca2d70a532d0cf
singh janmanpreet <janmanpreet.singh@gmail.com>
2022-12-15 14:10:03 +0100

    react-router-03 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./components/Welcome";
 import { Counter } from "./components/Counter";
+import { GithubUser } from "./components/GithubUser";
 
 export function App() {
   return (
@@ -10,6 +11,7 @@ export function App() {
       <Routes>
         <Route path="/" element={<Welcome name="janmanpreet" />} />
         <Route path="/counter" element={<Counter />} />
+        <Route path="/user/:username" element={<GithubUser />} />
       </Routes>
     </div>
   );
