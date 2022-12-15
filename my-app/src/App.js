607aadd09aa5ad4d82ea992117f11ab6c7793d0e
singh janmanpreet <janmanpreet.singh@gmail.com>
2022-12-15 14:02:49 +0100

    react-router-02 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import "./index.css";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./components/Welcome";
+import { Counter } from "./components/Counter";
 
 export function App() {
   return (
     <div className="container">
       <Routes>
         <Route path="/" element={<Welcome name="janmanpreet" />} />
+        <Route path="/counter" element={<Counter />} />
       </Routes>
     </div>
   );
