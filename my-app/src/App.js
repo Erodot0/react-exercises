905e4df913169924d1df265729dfe3785ec93227
singh janmanpreet <janmanpreet.singh@gmail.com>
2022-12-15 14:23:01 +0100

    react-router-04 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,19 @@
 import "./index.css";
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { Welcome } from "./components/Welcome";
 import { Counter } from "./components/Counter";
 import { ShowGithubUser } from "./ShowGIthubUser";
 
+
 export function App() {
   return (
     <div className="container">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/counter">counter</Link>
+        <Link to="/user">User</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Welcome name="janmanpreet" />} />
         <Route path="/counter" element={<Counter />} />
